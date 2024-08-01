c062f03c134eb34115d949c2bd7841efee0d7669
STK <thoshithkautilya@yahoo.in>
2024-08-01 23:17:47 +0530

    Changed appearance

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -86,6 +86,8 @@ dependencies {
 
     implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
     implementation ("com.google.code.gson:gson:2.8.8")
+    implementation ("com.google.android.material:material:1.7.0")
+
 
 }
 
